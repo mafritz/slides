8ef5b87a6665d2b0dfcca244286f99652280110c
Mattia A. Fritz <mattia.fritz@unige.ch>
2023-11-01 22:23:22 +0100

    Speech recognition check

--- fr/interactivite-avec-js/js/speech-recognition.js
+++ fr/interactivite-avec-js/js/speech-recognition.js
@@ -1,43 +1,57 @@
 // JavaScript
-var recognition = new (window.SpeechRecognition ||
-  window.webkitSpeechRecognition ||
-  window.mozSpeechRecognition ||
-  window.msSpeechRecognition)();
-recognition.lang = "en";
-recognition.interimResults = false;
-recognition.maxAlternatives = 1;
+if (
+  !(
+    "SpeechRecognition" in window ||
+    "webkitSpeechRecognition" in window ||
+    "mozSpeechRecognition" in window ||
+    "msSpeechRecognition" in window
+  )
+) {
+  document.getElementById("startButton").style.display = "none";
+  document.getElementById("notWorking").style.display = "block";
+} else {
+  document.getElementById("startButton").style.display = "inline";
+  document.getElementById("notWorking").style.display = "none";
+  var recognition = new (window.SpeechRecognition ||
+    window.webkitSpeechRecognition ||
+    window.mozSpeechRecognition ||
+    window.msSpeechRecognition)();
+  recognition.lang = "en";
+  recognition.interimResults = false;
+  recognition.maxAlternatives = 1;
 
-recognition.addEventListener("start", function () {
-  document.getElementById("message").textContent = "I am listening..."; // replace with your 'listening' icon
-  document.getElementById("output").textContent = "";
-});
+  recognition.addEventListener("start", function () {
+    document.getElementById("message").textContent = "I am listening..."; // replace with your 'listening' icon
+    document.getElementById("output").textContent = "";
+  });
 
-recognition.addEventListener("end", function () {
-  document.getElementById("message").textContent = "Say the color out loud"; // replace with your original icon
-});
+  recognition.addEventListener("end", function () {
+    document.getElementById("message").textContent = "Say the color out loud"; // replace with your original icon
+  });
 
-document.getElementById("startButton").addEventListener("click", function () {
-  recognition.start();
-});
+  document.getElementById("startButton").addEventListener("click", function () {
+    recognition.start();
+  });
 
-// JavaScript
-var colors = ["red", "blue", "green", "orange", "purple"]; // replace with your colors
+  // JavaScript
+  var colors = ["red", "blue", "green", "orange", "purple"]; // replace with your colors
 
-recognition.addEventListener("result", function (event) {
-  var transcript = event.results[0][0].transcript.toLowerCase();
-  transcript = transcript.replace(/\.$/, ""); // strip the period at the end
-  var words = transcript.split(" ");
-  var outputText = words.join(" ");
+  recognition.addEventListener("result", function (event) {
+    var transcript = event.results[0][0].transcript.toLowerCase();
+    transcript = transcript.replace(/\.$/, ""); // strip the period at the end
+    var words = transcript.split(" ");
+    var outputText = words.join(" ");
 
-  // check if the spoken word is a color
-  var colorWord = words.find((word) => colors.includes(word));
-  if (colorWord) {
-    // change the color of the text to the spoken color
-    document.getElementById("output").textContent = outputText;
-    document.getElementById("output").style.color = colorWord;
-  } else {
-    document.getElementById("output").textContent =
-      "Sorry, I did not recognize any of these colors!";
-    document.getElementById("output").style.color = "black";
-  }
-});
+    // check if the spoken word is a color
+    var colorWord = words.find((word) => colors.includes(word));
+    if (colorWord) {
+      // change the color of the text to the spoken color
+      document.getElementById("output").textContent = outputText;
+      document.getElementById("output").style.color = colorWord;
+    } else {
+      document.getElementById("output").textContent =
+        "Sorry, I did not recognize any of these colors!";
+      document.getElementById("output").style.color = "black";
+    }
+  });
+}
